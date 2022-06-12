6c8ab04156c218b1a937520f8fd050ba8b5995f3
jeyraj142 <107271816+jeyraj142@users.noreply.github.com>
2022-06-12 02:11:12 -0400

    version 1.2

--- wp-content/themes/pathway/header.php
+++ wp-content/themes/pathway/header.php
@@ -7,6 +7,24 @@
 </head>
 
 <body id="kubio" <?php body_class(); ?>>
+	<!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+96897562075", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            pre_filled_message: "Hi welcome to Al Majara Elevators happy to help u", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
+	
 <?php
 if ( function_exists( 'wp_body_open' ) ) {
 	wp_body_open();
